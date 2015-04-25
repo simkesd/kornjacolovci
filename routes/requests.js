61d68ff394c6ce9f304c60969aa9c290cc70360c
Milos Simic <simkesd@gmail.com>
2015-04-25 20:29:57 +0200

    added requests/active route

--- routes/requests.js
+++ routes/requests.js
@@ -7,6 +7,7 @@ var APIService    = require(__dirname + '/../lib/APIService');
 api = new APIService();
 
 router.get('/:id?', api.requests);
+router.get('/active', api.requestsActive);
 router.post('/', api.requestsAdd);
 router.put('/:id', api.requestsEdit);
 router.delete('/:id', api.requestsDelete);
