1de1358bcfeb698eecaf7f492c5ad6bffd6baefb
theZivkovic <dejan.zivkovic90@gmail.com>
2015-04-25 19:50:02 +0200

    service getMyRequests

--- public/app/js/services/service.js
+++ public/app/js/services/service.js
@@ -2,7 +2,8 @@ kornjacolovciApp.factory('FactoryTest', ['$resource', function($resource){
 	return $resource("", {}, {
 		getUserList :	{ url:"http://localhost:3000/users/userlist", params: {}, method:"GET" , isArray:true},
 		addUser	    :   { url:"http://localhost:3000/users/addUser" , params: {username:"@username"}, method:"POST", isArray:false},
-		getRequestList : { url : '/requests/active', params: {}, method: 'GET',  timeout : 5000, isArray:true}
+		getRequestList : { url : '/requests/active', params: {}, method: 'GET',  timeout : 5000, isArray:true},
+		getMyRequests : {url: '/requests/mine', params: {user_id: "@user_id" }, method: 'GET', timeout: 5000, isArray: true }
 	});
 }]);
 
