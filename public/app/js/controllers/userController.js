0148ece63055ea7ba7530ee42550365a8f85dd08
theZivkovic <dejan.zivkovic90@gmail.com>
2015-04-25 22:46:36 +0200

    prvi post

--- public/app/js/controllers/userController.js
+++ public/app/js/controllers/userController.js
@@ -6,11 +6,27 @@ kornjacolovciApp.controller('userController', ['$scope', '$routeParams', 'Factor
      	$location.path('/user/' + $routeParams.id + '/requests');
      }
 
+     $scope.addMyRequest = function(){
+     	FactoryTest.addMyRequest({
+     		animal_ID : 1,
+     		quantity: $scope.quantity,
+     		weight: $scope.weight,
+     		delivery_date: $scope.deliveryDate,
+     		user_ID : $routeParams.id,
+     		offer_ID : null,
+     		creation_time: null
+     	}, function(response){
+     		console.log(response);
+     	});
+     }
+
 	 var getMyRequests = function(){
      	FactoryTest.getMyRequests({id : $scope.id}, function(data){
      			$scope.requests = data;   		
      	});
      }
+
+
 
      getMyRequests();
     
--- public/app/js/services/service.js
+++ public/app/js/services/service.js
@@ -5,7 +5,9 @@ kornjacolovciApp.factory('FactoryTest', ['$resource', function($resource){
 		getAllRequestList : { url : '/requests/', params: {}, method: 'GET',  timeout : 5000, isArray:true},
 		getActiveRequestList : { url : '/requests/active', params: {}, method: 'GET',  timeout : 5000, isArray:true},
 		getMyRequests : {url: '/requests/:id', params: {}, method: 'GET', timeout: 5000, isArray:true},
-		getRequestDetails : {url : '/requests/:request_id', params : {}, method: 'GET', timeout: 5000, isArray:false}
+		addMyRequest : {url: '/requests', params: {}, method: 'POST', timeout: 5000},
+		getRequestDetails : {url : '/requests/:request_id', params : {}, method: 'GET', timeout: 5000, isArray:false},
+
 	});
 }]);
 
