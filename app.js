d248cfdf3d8d3197be7d8ff265d4951fa3f8e49d
Aleksandar Marić <aleksandar.maric@paragraf.rs>
2015-04-25 18:33:00 +0200

    hotfix

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/requests', );
-app.use('/offers', );
+app.use('/requests', function(){});
+app.use('/offers', function(){});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
