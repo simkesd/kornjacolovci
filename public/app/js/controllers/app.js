d256b6c16a3942abfa5971739c7ee406dce7b384
Aleksandar Marić <aleksandar.maric@paragraf.rs>
2015-04-26 07:04:48 +0200

    new sell offers

--- public/app/js/controllers/app.js
+++ public/app/js/controllers/app.js
@@ -38,6 +38,10 @@ kornjacolovciApp.config(function($routeProvider){
             templateUrl: 'views/login.html',
             controller: 'baseController'
         })
+        .when('/newSellOffer', {
+            templateUrl: 'views/newSellOffer.html',
+            controller: 'sellOfferController'
+        })
 	.otherwise('/');
 });
 
